6e235e76ae79d4588a3b2c61d59a28f258a93d99
jsredmond <110986047+jsredmond@users.noreply.github.com>
2023-01-05 08:55:54 -0500

    Added check for versioning

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ for bucket in buckets:
         bucket = client.get_bucket_encryption(Bucket=bucket)
     except client.exceptions.ClientError:
         print('The S3 bucket %s has no encryption configured!' % bucket)
+
+for bucket in client.list_buckets()['Buckets']:
+  bucket = bucket['Name']
+  response = client.get_bucket_versioning(Bucket=bucket)
+  if 'Status' in response and response['Status'] == 'Enabled':
+    print('The S3 bucket %s does not have versioning configured!' % bucket)
